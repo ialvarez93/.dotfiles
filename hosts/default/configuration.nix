ab76057c2bed54df06a18d0e7659353d507ecd3a
Ivan Alvarez <ivalvarez22@gmail.com>
2024-07-11 15:16:43 -0400

    feat(Channel): :arrow_up: Change channel to 24.05

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -101,10 +101,12 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.displayManager.defaultSession = "plasmax11";
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "ivan";
+  services.displayManager = {
+    sddm.enable = true;
+    defaultSession = "plasmax11";
+    autoLogin.enable = true;
+    autoLogin.user = "ivan";
+  };
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
